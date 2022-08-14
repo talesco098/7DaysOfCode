4c89197438f18dda81e0201c128494a9986a385f
Tales de Melo <melotales@hotmail.com>
2022-08-14 11:42:31 -0300

    Arrumando pontuação

--- OperadoresComparacao.js
+++ OperadoresComparacao.js
@@ -6,19 +6,19 @@ let numeroDez = 10
 let stringDez = '10'
 
 if(numeroUm == stringUm){
-  console.log("As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes")
+  console.log("As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes.")
 } else{
-  console.log("As variáveis numeroUm e stringUM não tem o mesmo valor")
+  console.log("As variáveis numeroUm e stringUM não tem o mesmo valor.")
 }
 
 if(numeroTrinta === stringTrinta){
-  console.log("As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo")
+  console.log("As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo.")
 } else{
-  console.log("As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo")
+  console.log("As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo.")
 }
 
 if(numeroDez == stringDez){
-  console.log("As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes")
+  console.log("As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes.")
 } else{
-  console.log("As variáveis numeroDez e stringDez não tem o mesmo valor")
+  console.log("As variáveis numeroDez e stringDez não tem o mesmo valor.")
 }
