737a03c0dd7be46d624ba143f5fa776fa9a8ee5b
Tales de Melo <melotales@hotmail.com>
2022-08-14 12:17:27 -0300

    Creating RecebendoImprindoMensagens

---
+++ RecebendoImprimindoMensagens.js
@@ -0,0 +1,5 @@
+const nome = prompt("Qual o seu nome?")
+const idade = prompt("Qual sua idade?")
+const linguagem = prompt("Qual linguagem de programação você está estudando?")
+
+alert("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem)
